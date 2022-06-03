c965bbde23515f0b34ee4167f47628c074d15818
Jekis11 <kogyxar94@gmail.com>
2022-06-03 20:08:45 +0300

    Notiz APP

--- app/src/main/java/com/example/notiz/CreateNotizFragment.kt
+++ app/src/main/java/com/example/notiz/CreateNotizFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_create_notiz.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class CreateNotizFragment : Fragment() {
@@ -32,5 +36,51 @@ class CreateNotizFragment : Fragment() {
 
                 }
             }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val cor = SimpleDateFormat("dd/M/yyyy hh:mm::ss")
+        val currentDatum = cor.format(Date())
+
+        tvDataTime.text = currentDatum
+
+
+        imgDone.setOnClickListener {
+            //speichern Notiz
+
+            speichern()
+        }
+
+        imgBack.setOnClickListener {
+        replaceFragment(HomeFragment.newInstance(),false)
+        }
+    }
+
+    private fun speichern(){
+
+        if(ednotiztitel.text.isNullOrEmpty()){
+            Toast.makeText(context,"Notiz Titel ist erforderlich", Toast.LENGTH_SHORT).show()
+        }
+
+        if (ednotizSubTitel.text.isNullOrEmpty()){
+            Toast.makeText(context,"Notiz Untertitel ist erforderlich", Toast.LENGTH_SHORT).show()
+        }
+
+
+        if (ednotizDesc.text.isNullOrEmpty()){
+            Toast.makeText(context,"Notiz Beschreibung muss kann nicht null", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    fun replaceFragment(fragment: Fragment, istransition:Boolean){
+        val fragmentTransition = requireActivity().supportFragmentManager.beginTransaction()
+        if (istransition){
+            fragmentTransition.setCustomAnimations(android.R.anim.slide_out_right,android.R.anim.slide_in_left)
+        }
+        fragmentTransition.add(R.id.frame_layout,fragment).addToBackStack(fragment.javaClass.simpleName).commit()
+
     }
 }
--- app/src/main/java/com/example/notiz/HomeFragment.kt
+++ app/src/main/java/com/example/notiz/HomeFragment.kt
@@ -5,10 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_home.*
 
 
 class HomeFragment : Fragment() {
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -33,4 +34,24 @@ class HomeFragment : Fragment() {
                 }
             }
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fabBtnCreateNotiz.setOnClickListener {
+
+            replaceFragment(CreateNotizFragment.newInstance(),true)
+
+        }
+    }
+
+    fun replaceFragment(fragment: Fragment, istransition:Boolean){
+    val fragmentTransition = requireActivity().supportFragmentManager.beginTransaction()
+    if (istransition){
+        fragmentTransition.setCustomAnimations(android.R.anim.slide_out_right,android.R.anim.slide_in_left)
+    }
+    fragmentTransition.add(R.id.frame_layout,fragment).addToBackStack(fragment.javaClass.simpleName).commit()
+
 }
+}
+
